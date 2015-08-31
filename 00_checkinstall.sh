2c0b9789e6576fefae52ad24dd0d24843735f286
Naftoli Gugenheim <naftoligug@gmail.com>
2015-08-31 00:31:56 -0400

    Check for existence of properties file separately so it can be volume-mounted

--- 00_checkinstall.sh
+++ 00_checkinstall.sh
@@ -4,14 +4,19 @@ if [ -z "$TEAMCITY_SERVER" ]; then
     exit 1
 fi
 
-if [ ! -d "$AGENT_DIR" ]; then
-    echo "$AGENT_DIR doesn't exist pulling build-agent from server $TEAMCITY_SERVER";
+if [ ! -d "$AGENT_DIR/bin" ]; then
+    echo "$AGENT_DIR/bin doesn't exist; pulling build agent from server $TEAMCITY_SERVER";
     wget $TEAMCITY_SERVER/update/buildAgent.zip
     unzip -d $AGENT_DIR buildAgent.zip
     rm buildAgent.zip
     chmod +x $AGENT_DIR/bin/agent.sh
-    echo "serverUrl=${TEAMCITY_SERVER}" > $AGENT_DIR/conf/buildAgent.properties
-    echo "workDir=/data/work" >> $AGENT_DIR/conf/buildAgent.properties
-    echo "tempDir=/data/temp" >> $AGENT_DIR/conf/buildAgent.properties
-    echo "systemDir=/data/system" >> $AGENT_DIR/conf/buildAgent.properties
+fi
+
+PROPS_FILE=$AGENT_DIR/conf/buildAgent.properties
+
+if [ ! -f "$PROPS_FILE" ]
+    echo "serverUrl=${TEAMCITY_SERVER}" > $PROPS_FILE
+    echo "workDir=/data/work" >> $PROPS_FILE
+    echo "tempDir=/data/temp" >> $PROPS_FILE
+    echo "systemDir=/data/system" >> $PROPS_FILE
 fi
