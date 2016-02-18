5b3ca22eaa7fbd0a34f23382d3a636732dbf4e52
Naftoli Gugenheim <naftoligug@gmail.com>
2016-02-18 00:47:22 -0500

    runagent.sh: Mount /etc/timezone:ro

--- runagent.sh
+++ runagent.sh
@@ -1,3 +1,12 @@
 NUM=$1
 
-docker run -v /var/tc-agent/$NUM/conf:/opt/buildAgent/conf -v /var/tc-agent/$NUM/data:/data --privileged --name=teamcity-agent-$NUM -e TEAMCITY_SERVER=http://10.132.21.200 -d --restart=always 1on1/teamcity-agent:latest
+docker run \
+  -v /etc/timezone:/etc/timezone:ro
+  -v /var/tc-agent/$NUM/conf:/opt/buildAgent/conf \
+  -v /var/tc-agent/$NUM/data:/data \
+  --privileged \
+  --name=teamcity-agent-$NUM \
+  --restart=always
+  -e TEAMCITY_SERVER=http://10.132.21.200 \
+  -d \
+  1on1/teamcity-agent:latest
