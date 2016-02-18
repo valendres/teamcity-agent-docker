4cd034470ff40bd4c1d475165ad8051d91f7d980
Naftoli Gugenheim <naftoligug@gmail.com>
2016-02-17 21:08:43 -0500

    runagent.sh: add --restart=always

--- runagent.sh
+++ runagent.sh
@@ -1,3 +1,3 @@
 NUM=$1
 
-docker run -v /var/tc-agent/$NUM/conf:/opt/buildAgent/conf -v /var/tc-agent/$NUM/data:/data --privileged --name=teamcity-agent-$NUM -e TEAMCITY_SERVER=http://10.132.21.200 -d 1on1/teamcity-agent:latest
+docker run -v /var/tc-agent/$NUM/conf:/opt/buildAgent/conf -v /var/tc-agent/$NUM/data:/data --privileged --name=teamcity-agent-$NUM -e TEAMCITY_SERVER=http://10.132.21.200 -d --restart=always 1on1/teamcity-agent:latest
